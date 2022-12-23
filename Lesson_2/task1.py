5aee9723a02275e5f996b63bfa147b22bf04bb0a
GoncharukOleksandr <116072099+GoncharukOleksandr@users.noreply.github.com>
2022-12-23 15:30:00 +0300

    Delete task1.py

--- Lesson_2/task1.py
+++
@@ -1,7 +0,0 @@
-Yourlist=input("Введіть ваш список: ")
-Lettercounts={}
-uniqueletters=set(Yourlist)
-for element in uniqueletters:
-    Lettercounts[element]=Yourlist.count(element)
-print("Ваш список: ")
-print(Lettercounts)
