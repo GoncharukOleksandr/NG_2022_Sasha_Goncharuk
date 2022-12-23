ddc23677e61d2ba61755304ae349aadf3ae0cb90
GoncharukOleksandr <116072099+GoncharukOleksandr@users.noreply.github.com>
2022-12-23 15:28:40 +0300

    Delete task3.py

--- Lesson_2/task3.py
+++
@@ -1,7 +0,0 @@
-Yournumber=int(input("Введіть число: "))
-print("Результат: ")
-while Yournumber>0:
-    for row in range(-Yournumber, 0):
-        print(-row,end=" ")
-    print()
-    Yournumber=Yournumber-1
