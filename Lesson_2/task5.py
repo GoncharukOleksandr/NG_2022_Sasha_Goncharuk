e6d4a1447b3a66066400a615d559462d98390c20
GoncharukOleksandr <116072099+GoncharukOleksandr@users.noreply.github.com>
2022-12-23 15:30:42 +0300

    Delete task5.py

--- Lesson_2/task5.py
+++
@@ -1,8 +0,0 @@
-YourNumbers=[]
-YourNumbers=input("Введіть ваш номер: ")
-YourNumbers=list(map(int,YourNumbers.split(",")))
-YourNumbers.sort()
-print(YourNumbers)
-print(min(YourNumbers))
-print(max(YourNumbers))
-print(sum(YourNumbers[1: -1]))
