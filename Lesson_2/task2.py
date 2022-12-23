b44d1b08968abc74eb37e55a5a4cd8fcbd9baa7f
GoncharukOleksandr <116072099+GoncharukOleksandr@users.noreply.github.com>
2022-12-23 15:28:29 +0300

    Delete task2.py

--- Lesson_2/task2.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-
-
-my_list = ["VAG", "Mercedes", "BMW", "VAG", "Audi", "Wolkswagen", "Skoda", "Bentley"]
-print("Оригінальний список: " + str(my_list))
-
-li = list(OrderedDict.fromkeys(my_list))
-print("Список після видалення дублікатів: " + str(li))
